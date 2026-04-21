4bf3aa723a1e87eaa716845432f5d9e0cff67708
ian-bird <ianukusa@gmail.com>
2026-04-21 04:13:18 -0500

    add example of useValue

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func evenNum(i int) int {
 	return i
 }
 
+func oddNum(i int) int {
+	if i % 2 != 1 {
+		return condition.Error[int](fmt.Errorf("%v is not odd!", i))
+	}
+
+	return i
+}
+
 func main() {
 	// standard try/catch with dynamic unwinding
 	x := condition.HandlerCase(func() int {
@@ -42,9 +50,23 @@ func main() {
 		z = evenNum(3) * 3
 	})
 
+	// show useValue overriding erroneous value
+	var a int
+	condition.HandlerBind().Handler(func(e error) {
+		var i int
+		c, _ := fmt.Sscanf(e.Error(), "%v is not odd!", &i)
+		if c != 1 {
+			return
+		}
+
+		condition.UseValue[int](1)
+	}).Run(func() {
+		a = oddNum(2) * 5
+	})	
+
 	// show warnings going uncaught
 	var y int
 	y = evenNum(7)
 
-	fmt.Printf("%v %v %v\n", x, y, z)
+	fmt.Printf("%v %v %v %v\n", x, y, z, a) // prints 3 7 9 5 and warning for 7
 }
